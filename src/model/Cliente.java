a904718e70d20049781c83d921fd898ba5df16ba
OliveiraBeatr1z <luamorningstar3@gmail.com>
2025-05-12 17:33:03 -0300

    commit 1

---
+++ src/model/Cliente.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Cliente {
+	private String cpf;
+	private String nome;
+	private int idade;
+	private Double limiteCredito;
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Double getLimiteCredito() {
+		return limiteCredito;
+	}
+	public void setLimiteCredito(Double limiteCredito) {
+		this.limiteCredito = limiteCredito;
+	}
+	@Override
+	public String toString() {
+		return nome + ";" + idade + ";" + limiteCredito;
+	}
+	
+	
+	
+
+	
+	
+}
